db03a5ede49c925c71327646091d1987317e77de
taro_kimura <taro_kimura@occ.co.jp>
2014-07-09 16:04:07 +0900

    C#勉強用

---
+++ referee/form_referee.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace referee
+{
+    partial class referee
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_input = new System.Windows.Forms.TextBox();
+            this.butt_check = new System.Windows.Forms.Button();
+            this.butt_close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text_input
+            // 
+            this.text_input.Location = new System.Drawing.Point(12, 12);
+            this.text_input.Name = "text_input";
+            this.text_input.Size = new System.Drawing.Size(197, 19);
+            this.text_input.TabIndex = 0;
+            // 
+            // butt_check
+            // 
+            this.butt_check.Location = new System.Drawing.Point(12, 37);
+            this.butt_check.Name = "butt_check";
+            this.butt_check.Size = new System.Drawing.Size(96, 34);
+            this.butt_check.TabIndex = 1;
+            this.butt_check.Text = "チェック";
+            this.butt_check.UseVisualStyleBackColor = true;
+            this.butt_check.Click += new System.EventHandler(this.butt_check_Click);
+            // 
+            // butt_close
+            // 
+            this.butt_close.Location = new System.Drawing.Point(114, 37);
+            this.butt_close.Name = "butt_close";
+            this.butt_close.Size = new System.Drawing.Size(95, 34);
+            this.butt_close.TabIndex = 2;
+            this.butt_close.Text = "おしまい";
+            this.butt_close.UseVisualStyleBackColor = true;
+            this.butt_close.Click += new System.EventHandler(this.butt_close_Click);
+            // 
+            // referee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 81);
+            this.Controls.Add(this.butt_close);
+            this.Controls.Add(this.butt_check);
+            this.Controls.Add(this.text_input);
+            this.Name = "referee";
+            this.Text = "レフェリー";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_input;
+        private System.Windows.Forms.Button butt_check;
+        private System.Windows.Forms.Button butt_close;
+    }
+}
+
---
+++ referee/form_referee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace referee
+{
+    public partial class referee : Form
+    {
+        public referee()
+        {
+            InitializeComponent();
+        }
+
+        // [チェック]ボタンが押された場合 : TextBox内の文字列が数値かどうかチェックする。
+        private void butt_check_Click(object sender, EventArgs e)
+        {
+            // 文字列が入力されていない場合
+            if (text_input.Text == "")
+            {
+                MessageBox.Show("文字を入力してください");
+                text_input.Focus();
+                return;
+            }
+
+            // 文字列が数値かどうかをチェックする
+            if (Validation.IsNumeric(text_input.Text))
+            {
+                MessageBox.Show("数値です");
+                return;
+            }
+            else
+            {
+                MessageBox.Show("数値ではありません");
+                return;
+            }
+        }
+
+        private void butt_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    ///     検証・エラーチェックをサポートした静的クラスです。
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+
+    public sealed class Validation
+    {
+
+        #region　IsNumeric メソッド (+1)
+
+        /// -----------------------------------------------------------------------------
+        /// <summary>
+        ///     文字列が数値であるかどうかを返します。</summary>
+        /// <param name="stTarget">
+        ///     検査対象となる文字列。<param>
+        /// <returns>
+        ///     指定した文字列が数値であれば true。それ以外は false。</returns>
+        /// -----------------------------------------------------------------------------
+        public static bool IsNumeric(string stTarget)
+        {
+            double dNullable;
+            bool flg_num;
+
+            flg_num = double.TryParse(stTarget,System.Globalization.NumberStyles.Any,null,out dNullable);
+
+            return flg_num;
+        }
+
+        #endregion
+
+    }
+}
