d4c1e8ccbbcc905a408ffb952f14600419382b71
taro_kimura <taro_kimura@occ.co.jp>
2014-07-10 11:53:12 +0900

    日付チェック追加

--- referee/form_referee.Designer.cs
+++ referee/form_referee.Designer.cs
@@ -29,44 +29,56 @@
         private void InitializeComponent()
         {
             this.text_input = new System.Windows.Forms.TextBox();
-            this.butt_check = new System.Windows.Forms.Button();
+            this.butt_check_num = new System.Windows.Forms.Button();
             this.butt_close = new System.Windows.Forms.Button();
+            this.butt_check_date = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // text_input
             // 
             this.text_input.Location = new System.Drawing.Point(12, 12);
             this.text_input.Name = "text_input";
-            this.text_input.Size = new System.Drawing.Size(197, 19);
+            this.text_input.Size = new System.Drawing.Size(198, 19);
             this.text_input.TabIndex = 0;
             // 
-            // butt_check
+            // butt_check_num
             // 
-            this.butt_check.Location = new System.Drawing.Point(12, 37);
-            this.butt_check.Name = "butt_check";
-            this.butt_check.Size = new System.Drawing.Size(96, 34);
-            this.butt_check.TabIndex = 1;
-            this.butt_check.Text = "チェック";
-            this.butt_check.UseVisualStyleBackColor = true;
-            this.butt_check.Click += new System.EventHandler(this.butt_check_Click);
+            this.butt_check_num.Location = new System.Drawing.Point(12, 37);
+            this.butt_check_num.Name = "butt_check_num";
+            this.butt_check_num.Size = new System.Drawing.Size(96, 34);
+            this.butt_check_num.TabIndex = 1;
+            this.butt_check_num.Text = "数値チェック";
+            this.butt_check_num.UseVisualStyleBackColor = true;
+            this.butt_check_num.Click += new System.EventHandler(this.butt_check_num_Click);
             // 
             // butt_close
             // 
-            this.butt_close.Location = new System.Drawing.Point(114, 37);
+            this.butt_close.Location = new System.Drawing.Point(114, 77);
             this.butt_close.Name = "butt_close";
-            this.butt_close.Size = new System.Drawing.Size(95, 34);
+            this.butt_close.Size = new System.Drawing.Size(96, 34);
             this.butt_close.TabIndex = 2;
             this.butt_close.Text = "おしまい";
             this.butt_close.UseVisualStyleBackColor = true;
             this.butt_close.Click += new System.EventHandler(this.butt_close_Click);
             // 
+            // butt_check_date
+            // 
+            this.butt_check_date.Location = new System.Drawing.Point(114, 37);
+            this.butt_check_date.Name = "butt_check_date";
+            this.butt_check_date.Size = new System.Drawing.Size(96, 34);
+            this.butt_check_date.TabIndex = 3;
+            this.butt_check_date.Text = "日付チェック";
+            this.butt_check_date.UseVisualStyleBackColor = true;
+            this.butt_check_date.Click += new System.EventHandler(this.butt_check_date_Click);
+            // 
             // referee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(221, 81);
+            this.ClientSize = new System.Drawing.Size(224, 119);
+            this.Controls.Add(this.butt_check_date);
             this.Controls.Add(this.butt_close);
-            this.Controls.Add(this.butt_check);
+            this.Controls.Add(this.butt_check_num);
             this.Controls.Add(this.text_input);
             this.Name = "referee";
             this.Text = "レフェリー";
@@ -78,8 +90,9 @@
         #endregion
 
         private System.Windows.Forms.TextBox text_input;
-        private System.Windows.Forms.Button butt_check;
+        private System.Windows.Forms.Button butt_check_num;
         private System.Windows.Forms.Button butt_close;
+        private System.Windows.Forms.Button butt_check_date;
     }
 }
 
--- referee/form_referee.cs
+++ referee/form_referee.cs
@@ -17,12 +17,12 @@ namespace referee
         }
 
         // [チェック]ボタンが押された場合 : TextBox内の文字列が数値かどうかチェックする。
-        private void butt_check_Click(object sender, EventArgs e)
+        private void butt_check_num_Click(object sender, EventArgs e)
         {
             // 文字列が入力されていない場合
             if (text_input.Text == "")
             {
-                MessageBox.Show("文字を入力してください");
+                MessageBox.Show("文字を入力してください。");
                 text_input.Focus();
                 return;
             }
@@ -30,12 +30,61 @@ namespace referee
             // 文字列が数値かどうかをチェックする
             if (Validation.IsNumeric(text_input.Text))
             {
-                MessageBox.Show("数値です");
+                MessageBox.Show("数値です。");
                 return;
             }
             else
             {
-                MessageBox.Show("数値ではありません");
+                MessageBox.Show("数値ではありません。");
+                return;
+            }
+        }
+
+        private void butt_check_date_Click(object sender, EventArgs e)
+        {
+            // 年月日
+            int int_year;
+            int int_month;
+            int int_day;
+
+            bool bool_year;
+            bool bool_month;
+            bool bool_day;
+
+            // 文字列が入力されていない場合
+            if (text_input.Text == "")
+            {
+                MessageBox.Show("文字を入力してください。");
+                text_input.Focus();
+                return;
+            }
+
+            // 入力された数値が8桁の場合 : 入力された8桁の数値が日付かどうかチェック
+            if (text_input.Text.Length == 8)
+            {
+                bool_year = int.TryParse(text_input.Text.Substring(0,4), System.Globalization.NumberStyles.Any, null, out int_year);
+                bool_month = int.TryParse(text_input.Text.Substring(4,2), System.Globalization.NumberStyles.Any, null, out int_month);
+                bool_day = int.TryParse(text_input.Text.Substring(6,2), System.Globalization.NumberStyles.Any, null, out int_day);
+
+                if (bool_year && bool_month && bool_day)
+                {
+                    if (Validation.IsDate(int_year, int_month, int_day))
+                    {
+                        MessageBox.Show("正しい日付です。");
+                    }
+                    else
+                    {
+                        MessageBox.Show("正しい日付ではありません。");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("日付の中に文字が含まれています。");
+                }
+            }
+            else
+            {
+                MessageBox.Show("8桁の数値を入力してください。例) 20140710");
                 return;
             }
         }
@@ -44,6 +93,7 @@ namespace referee
         {
             this.Close();
         }
+        
     }
 
     /// -----------------------------------------------------------------------------
@@ -73,6 +123,44 @@ namespace referee
             flg_num = double.TryParse(stTarget,System.Globalization.NumberStyles.Any,null,out dNullable);
 
             return flg_num;
+        }
+
+        #endregion
+
+        #region　IsDate メソッド
+
+        /// ------------------------------------------------------------------------------
+        /// <summary>
+        ///     指定した年・月・日が正しい日付であるかどうかを返します。</summary>
+        /// <param name="iYear">
+        ///     検査対象となる年。</param>
+        /// <param name="iMonth">
+        ///     検査対象となる月。</param>
+        /// <param name="iDay">
+        ///     検査対象となる日。</param>
+        /// <returns>
+        ///     指定した年・月・日が正しい日付であれば true。それ以外は false。</returns>
+        /// ------------------------------------------------------------------------------
+        public static bool IsDate(int iYear, int iMonth, int iDay)
+        {
+            if ((DateTime.MinValue.Year > iYear) || (iYear > DateTime.MaxValue.Year))
+            {
+                return false;
+            }
+
+            if ((DateTime.MinValue.Month > iMonth) || (iMonth > DateTime.MaxValue.Month))
+            {
+                return false;
+            }
+
+            int iLastDay = DateTime.DaysInMonth(iYear, iMonth);
+
+            if ((DateTime.MinValue.Day > iDay) || (iDay > iLastDay))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         #endregion
