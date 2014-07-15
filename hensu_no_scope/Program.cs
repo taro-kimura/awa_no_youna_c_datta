e167f46beb9aab0dc4d7bfb6349e8a03afe56cb0
taro_kimura <taro_kimura@occ.co.jp>
2014-07-15 13:09:39 +0900

    変数のスコープを学習: 完了

--- hensu_no_scope/Program.cs
+++ hensu_no_scope/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace hensu_no_scope
 {
     class Program
     {
+        private static int a = 1;
         static void Main(string[] args)
         {
-
+            int b = 2;
+            {
+                int s = a + b;
+                // 変数sはスコープの中なので使用できる
+                Console.WriteLine(s);
+            }
+            // 変数sはスコープの外なのでコンパイルエラーになる
+            // Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
