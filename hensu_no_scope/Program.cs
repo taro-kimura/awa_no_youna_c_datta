e7632f4205ed0a906f22baedc233fe20d402baf0
taro_kimura <taro_kimura@occ.co.jp>
2014-07-15 12:57:04 +0900

    変数のスコープを学習

---
+++ hensu_no_scope/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hensu_no_scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
