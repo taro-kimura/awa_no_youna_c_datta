682a876ef22a4cd375fd3b57a055b53324a1714b
taro_kimura <taro_kimura@occ.co.jp>
2014-07-15 14:16:40 +0900

    ArrayListの学習: 完了

---
+++ collection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // aの宣言
+            ArrayList a = new ArrayList();
+            
+            // 入園
+            a.Add("いとうあい");
+            a.Add("ひらのりほ");
+
+            Console.WriteLine("- リス組園児リスト (2013年4月) -");
+
+            // aを出力
+            foreach (string x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+            // 入園: あらいあきこ 
+            a.Insert(0,"あらいあきこ");
+
+            Console.WriteLine("- リス組園児リスト (2013年7月) -");
+
+            // aを出力
+            foreach (string x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+            // 卒園: いとうあい
+            a.RemoveAt(1);
+
+            Console.WriteLine("- リス組園児リスト (2014年3月) -");
+
+            // aを出力
+            foreach (string x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+            // [Enter]で終了
+            Console.ReadLine();
+
+        }
+    }
+}
