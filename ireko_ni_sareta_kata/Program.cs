f8178fe607cefdbdef4743af68739b80d22435c4
taro_kimura <taro_kimura@occ.co.jp>
2014-07-16 16:38:37 +0900

    入れ子にされた型: 完了

---
+++ ireko_ni_sareta_kata/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ireko_ni_sareta_kata
+{
+    class Program
+    {
+        // 入れ子にされた型
+        // SampleクラスはProgramクラスの中だけで使用できる
+        class Sample
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public void Show()
+            {
+                Console.Write(Name);
+                Console.Write(" ￥");
+                Console.Write(Price);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // 同じクラスのメンバーであれば、Sampleクラスを一般的に宣言されたクラスと同様に使用できる
+            Sample a = new Sample();
+
+            a.Name = "焼きそばパン";
+            a.Price = 150;
+            a.Show();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ireko_ni_sareta_kata/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("ireko_ni_sareta_kata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ireko_ni_sareta_kata")]
+[assembly: AssemblyCopyright("Copyright © HP 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("e777f107-df9c-4d44-8677-b30c63d6a9a3")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
