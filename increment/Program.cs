17484dc8b36eddc1e4774a6a9f58a4ce5953f582
taro_kimura <taro_kimura@occ.co.jp>
2014-07-16 16:03:51 +0900

    「i++」と「++i」の違い

---
+++ increment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace increment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // i++
+            int i = 0;
+            int a = i++;
+
+            // i++は値を返して、「1」加える
+            Console.WriteLine("i = " + i + ", i++ = " + a);
+
+            // ++i
+            int j = 0;
+            int b = ++j;
+
+            // ++iは、「1」加えて値を返す
+            Console.WriteLine("i = " + j + ", ++i = " + b);
+
+            Console.ReadLine();
+        }
+    }
+}
