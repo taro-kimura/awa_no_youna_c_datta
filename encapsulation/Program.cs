abd032f57639a406dc5601eeec3ef2c1063d2ee5
taro_kimura <taro_kimura@occ.co.jp>
2014-07-18 14:50:16 +0900

    カプセル化: 完了

---
+++ encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample a = new Sample();
+            a.Method();
+            Console.ReadLine();
+        }
+    }
+
+    // publicキーワードはクラスとメンバの宣言時に修飾することができる
+    public class Sample
+    {
+        // コンストラクタ
+        public Sample() { }
+        
+        // メソッド
+
+        // publicキーワードで修飾されたメンバはアクセスに制限はない
+        public void Method()
+        {
+            // MethodメソッドとPropertyプロパティは
+            // どちらもSampleクラスに記述されたメンバなので、
+            // お互いにアクセスすることができる
+            Property = 0;
+
+            Console.WriteLine("カプセル化");
+        }
+        
+        // プロパティ
+        // privateキーワードで修飾されたメンバは、
+        // メンバが宣言されているクラス、または構造体の内部からのみアクセス可能
+        private int Property { get; set; }
+    }
+}
