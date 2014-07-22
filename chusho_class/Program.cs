3465bf58f30a5c7ea650fdec979436c8030bc579
taro_kimura <taro_kimura@occ.co.jp>
2014-07-22 14:54:21 +0900

    抽象クラス: 完了

---
+++ chusho_class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chusho_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new SpadeAce();
+            card.Show();
+        }
+    }
+
+    // 抽象クラス
+    // Cardクラスは、派生クラスを宣言することによって、
+    // スペードのエース以外にもスペードのキングやハートのクイーンなど、
+    // 複数のマークと数を表現することができる
+    abstract class Card
+    {
+        // マークと数を表すプロパティが抽象化されている
+        // 抽象化されたクラスやメンバーの宣言は、abstractキーワードで修飾される
+        abstract public string Mark { get; }
+        abstract public string Number { get; }
+        public void Show()
+        {
+            Console.Write(Mark);
+            Console.Write(Number);
+            Console.ReadLine();
+        }
+    }
+
+    // Cardクラスを継承した派生クラス
+    // SpaceAceクラスは「スペードのエース」という特定のマークと数しか表現できない
+    class SpadeAce : Card
+    {
+        public override string Mark
+        {
+            get { return "スペード"; }
+        }
+
+        public override string Number
+        {
+            get { return "A"; }
+        }
+    }
+}
