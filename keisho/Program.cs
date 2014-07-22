3dba3d5e8e1d9eaac07ffff7636ad09f721b1422
taro_kimura <taro_kimura@occ.co.jp>
2014-07-22 10:28:49 +0900

    継承: 完了

---
+++ keisho/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace keisho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // InheritsClassクラスだけを使用する
+            InheritsClass x = new InheritsClass();
+
+            // BaseClassクラスのShowBaseメソッドを実行
+            // InheritsClassクラスには、
+            // ShowBaseメソッドが宣言されていないが、
+            // BaseClassクラスを継承しているため、
+            // ShowBaseメソッドを自身のクラスメンバーのように使用することができる
+            x.ShowBase();
+
+            // InheritsClassクラスのShowメソッドを実行
+            x.Show();
+            
+            Console.ReadLine();
+        }
+    }
+
+    class BaseClass
+    {
+        // ShowBaseメソッドだけを宣言している
+        public void ShowBase()
+        {
+            Console.WriteLine("私は基本クラスのメンバーです");
+        }
+    }
+
+    // BaseClassクラスを継承している
+    class InheritsClass : BaseClass
+    {
+        // Showメソッドだけを宣言している
+        public void Show()
+        {
+            ShowBase();
+            Console.WriteLine("私は派生クラスのメンバーです");
+        }
+    }
+}
