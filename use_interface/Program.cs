5d101410ed565ae14e8c6ddfe490c703fb329bae
taro_kimura <taro_kimura@occ.co.jp>
2014-07-22 15:39:32 +0900

    インターフェイス: 完了

---
+++ use_interface/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace use_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A・B・Cの3つのクラスは、ICalculatorインターフェイスで継承している
+            ICalculator[] a = { new A(), new B(), new C() };
+            
+            foreach (ICalculator calc in a)
+            {
+                int r = calc.Execute(3, 2);
+                Console.WriteLine(r);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ICalculator
+    {
+        int Execute(int a, int b);
+    }
+
+    class A : ICalculator
+    {
+        public int Execute(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class B : ICalculator
+    {
+        public int Execute(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class C : ICalculator
+    {
+        public int Execute(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
