f384b38b58d3d62778691b8016ad96f689c1525f
taro_kimura <taro_kimura@occ.co.jp>
2014-07-23 13:22:20 +0900

    イベントの実装と登録: 完了

---
+++ event_no_jisso_to_toroku/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace event_no_jisso_to_toroku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample x = new Sample();
+
+            // x.Valueが変更されたとき、"!?"を表示するイベント
+            x.ValueChanged += (s, e) => Console.Write("!?");
+
+            // イベント実行前
+            Console.ReadLine();
+
+            // 値の変更 -> イベントの実行
+            x.Value = 1;
+
+            Console.ReadLine();
+        }
+    }
+
+    class Sample
+    {
+        public event EventHandler ValueChanged;
+        private int _value = 0;
+        public int Value
+        {
+            get{return _value;}
+            set
+            {
+                _value = value;
+                if (ValueChanged == null) return;
+                ValueChanged(this,EventArgs.Empty);
+            }
+        }
+    }
+}
