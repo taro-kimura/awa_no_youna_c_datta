20854c54af86486a7444f35514d5917204c6f4d1
taro_kimura <taro_kimura@occ.co.jp>
2014-07-24 11:29:01 +0900

    例外の再スロー

---
+++ reigai_no_sai_throw/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace reigai_no_sai_throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // ReadData実行
+                ReadData();
+            }
+            catch(Exception ex)
+            {
+                // キャッチした例外インスタンスのメッセージ
+                Console.WriteLine(ex.Message);
+
+                // 例外インスタンスに格納されている、
+                // 例外元となった例外インスタンスのメッセージ
+                Console.WriteLine(ex.InnerException.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        // AccessDatabaseメソッドを実行中に発生した例外をキャッチ
+        static void ReadData()
+        {
+            try
+            {
+                AccessDatabase();
+            }
+            catch (Exception ex)
+            {
+                // Exception型の例外インスタンスを生成して再スローしている
+                throw new Exception("アクセス失敗", ex);
+            }
+        }
+
+        static void AccessDatabase()
+        {
+            throw new Exception("データベースが壊れています");
+        }
+    }
+}
